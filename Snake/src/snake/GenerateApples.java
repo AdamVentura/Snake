1e2e311b17e5404b08e2071bd1abed09b27aa3c8
noahschlorf <32345835+noahschlorf@users.noreply.github.com>
2021-05-13 12:28:10 -0500

    Update GenerateApples.java

--- Snake/src/snake/GenerateApples.java
+++ Snake/src/snake/GenerateApples.java
@@ -1,14 +1,68 @@
-public int[][] GenerateApples() {
-		x = 0;
-		y = 0;
+public int[][] CheckApple()
+	{
+		do
+		{
+			GenerateApples();		
+			
+		}while(board[a][q]!=0);
 		
-			Random dice = new Random();
+		board[a][q] = 3;
+		
+		return board;
+	}
 	
-			x = dice.nextInt(10);
+	
+	
+	public void GenerateApples() {
+		Random dice = new Random();
+		q = 0;
+		a = 0;
+		
+         	q = dice.nextInt(10);
+		a = dice.nextInt(10);
+		
+		System.out.println(a + " " + q);
+		
+     } 
+     
+     
+     public int updateboard()
+	{
+		//for each
+		for(y=9;y>=0;y--)
+		{
+			for(x=0;x<10;x++)
+			{
+				if(x==a&&y==q)
+				{
+					board[x][y]=3;
+				}
+				else
+				{
+				board[x][y]=0;
+				}
+				
+				for(z=0;z< snaketailx.size(); z++)
+				{
+					board[snaketailx.get(z)][snaketaily.get(z)]=1;
+					
+					
+				}
 			
-			y = dice.nextInt(10);
-			
-			board[y][x] = 3;
-						
-			return board;
-}
+				
+			}
+		}
+		for(y=9;y>=0;y--)
+		{
+			for(x=0;x<10;x++)
+			{
+		if(playerheadx==x&&playerheady==y)
+				{
+					board[x][y]=2;
+				}
+		
+		
+			}
+		}
+		return death;
+	}
