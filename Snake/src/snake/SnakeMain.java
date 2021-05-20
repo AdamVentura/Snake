460ee03a62b953b17d54f33e3885996a45403aed
lopatluk000 <81254109+lopatluk000@users.noreply.github.com>
2021-05-20 11:59:34 -0500

    Update SnakeMain.java

--- Snake/src/snake/SnakeMain.java
+++ Snake/src/snake/SnakeMain.java
@@ -6,20 +6,11 @@ import java.util.*;
 import javax.swing.JFrame;
 
 public class SnakeMain extends JFrame {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) throws InterruptedException {
 		// Creates a JFrame object
 		SnakeMain frame = new SnakeMain();
-		
-		int death = 0;
-		
-		
-        
 		
         // Creates panel object of Snake class
     	SnakeGraphics panel = new SnakeGraphics();
@@ -27,26 +18,23 @@ public class SnakeMain extends JFrame {
         frame.getContentPane().add(panel, BorderLayout.CENTER);
         // Make frame visible
         frame.setVisible(true);
-       
-       
-        
-        panel.DoAll();
+        //play the entire game
+        panel.DoAll();  
         
 		}
 	
 	public SnakeMain() {
 		// Sets the size of the window
-        setSize(506, 532);
+        setSize(516, 539);
         setResizable(false);
-     	 
+     	
         // Exit application when window is closed
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+       
 	}
 				
 			
 			
 			
 }
-	
 	
