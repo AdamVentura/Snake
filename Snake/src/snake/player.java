ca93f97bbbefc884da6abb4402727609e00139ac
Adam Ventura <adamvictorventura@gmail.com>
2021-05-24 11:52:31 -0500

    Added comments and arrow keys

--- Snake/src/snake/player.java
+++ Snake/src/snake/player.java
@@ -10,19 +10,34 @@ public class player implements KeyListener{
     //change the direction that the player is moving
     public void keyPressed(KeyEvent e)
 	{
-    	
+    	// Get the key that the player pressed
 		int key = e.getKeyCode();
 		
+		// For the keys W, A, S, and D
 		if(key==KeyEvent.VK_W){
-			direction=1;
+			direction=3;
 		}
 		if(key==KeyEvent.VK_S){
-			direction=3;
+			direction=1;
 		}
 		if(key==KeyEvent.VK_A){
 			direction=2;
 		}
 		if(key==KeyEvent.VK_D){
+			direction=4;
+		}
+		
+		// For the arrow keys
+		if(key==KeyEvent.VK_UP){
+			direction=3;
+		}
+		if(key==KeyEvent.VK_DOWN){
+			direction=1;
+		}
+		if(key==KeyEvent.VK_LEFT){
+			direction=2;
+		}
+		if(key==KeyEvent.VK_RIGHT){
 			direction=4;
 		}
 		
